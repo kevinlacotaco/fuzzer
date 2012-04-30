8f25aaa8a38c71581b3e70fdb7c766188f59fcf1
Phil Huge <pmh7863@rit.edu>
2012-04-30 00:50:39 -0400

    changes

--- src/utils/FuzzyLogger.java
+++ src/utils/FuzzyLogger.java
@@ -1,5 +1,11 @@
 package utils;
 
+import java.util.Date;
+
 public class FuzzyLogger {
 
+	public static void logError(String message) {
+		System.err.println("SE331_LOGGED: " + new Date() + "::" + message);
+	}
+	
 }
--- src/utils/ResultsProcessor.java
+++ src/utils/ResultsProcessor.java
@@ -1,9 +1,15 @@
 package utils;
 
+import com.gargoylesoftware.htmlunit.WebResponse;
+
 public class ResultsProcessor {
 	
-	public static void process(String htmlData) {
-		
+	public static void processWebResponse(WebResponse response) {
+		if(response.getStatusCode() > 399) {
+			FuzzyLogger.logError(response.getStatusCode() + ": " + response.getStatusMessage());
+		}
 	}
+	
+	
 
 }
